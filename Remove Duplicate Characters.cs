24f1891d9fe5242349eee9cdfdd8a15db837629d
Mathi Azhagan <109063537+azhagan001@users.noreply.github.com>
2022-08-01 14:14:16 +0530

    Remove Duplicate Characters

---
+++ Remove Duplicate Characters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Duplicate_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Duplicate = "csharpcorner";
+            Console.WriteLine("Initial String : " + Duplicate);
+            var Character = new HashSet<char>(Duplicate);
+            Console.WriteLine("After Remove Duplicate Characters : ");
+            foreach (char d in Character)
+                Console.Write(d);
+            Console.WriteLine();
+                while (Duplicate.Length > 0)
+                {
+                    int find = 0;
+                    Console.Write(Duplicate[0] + " = ");
+                    for (int i = 0; i < Duplicate.Length; i++)
+                    {
+                        if (Duplicate[i] == Duplicate[0])
+                        {
+                            find++;
+                        }
+                    }
+                    Console.WriteLine(find);
+                    Duplicate = Duplicate.Replace(Duplicate[0].ToString(), string.Empty);
+                }
+            Console.ReadLine();
+        }
+    }
+}
